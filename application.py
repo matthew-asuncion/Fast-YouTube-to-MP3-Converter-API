262fdcb90500c1df31e1860eb89c61682d2716d3
Ricardo Deodutt <RicardoDeodutt@gmail.com>
2021-09-04 17:13:05 -0400

    Added intital files

---
+++ application.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+import helper
+
+application = app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def converter():
+    youtube_url = ""
+    if request.method == "POST" and "youtube_link" in request.form:
+        # Declare all variables.
+        youtube_url = str(request.form.get("youtube_link")).strip()
+        # print(youtube_url)
+        # https://www.youtube.com/watch?v=lUKGzvQj4bI.
+
+    # Extracting the ID from youtube_link
+    youtube_id = str(helper.id_grabber(youtube_url))
+
+    # Making the IFrame link with the Youtube URL
+    api_converter_link = "https://www.yt-download.org/api/button/mp3/" + youtube_id
+
+    return render_template(
+        "index.html",
+        youtube_url=youtube_url,
+        youtube_id=youtube_id,
+        api_converter_link=api_converter_link,
+    )
+
+
+# if __name__ == "__main__":
+## Use 'debug=True' to avoid resarting vs code
+## everytime there is an update in the code.
+
+# app.run(debug=True)
---
+++ requirements.txt
@@ -0,0 +1,7 @@
+click==8.0.1
+colorama==0.4.4
+Flask==2.0.1
+itsdangerous==2.0.1
+Jinja2==3.0.1
+MarkupSafe==2.0.1
+Werkzeug==2.0.1
